cdb2ed4e680dad4e81977ec7f59ba293cb369e89
vamsi viswanath <vamsiviswanath60@gmail.com>
2026-07-10 20:28:36 +0530

    Added chatbot and logout test cases

--- testcases/test_chatbot.py
+++ testcases/test_chatbot.py
@@ -1,17 +1,14 @@
 from pages.guvi_home_page import GuviHomePage
 from pages.guvi_chatbot_page import ChatbotPage
+import time
 
 
 def test_09_verify_dobby_assistant(driver):
 
     home_page = GuviHomePage(driver)
-
     chatbot_page = ChatbotPage(driver)
-
     home_page.open_home_page()
-
+    time.sleep(2)
     chatbot_page.click_chatbot()
-
     chatbot_page.switch_to_chatbot_frame()
-
     assert chatbot_page.is_mini_visible()
--- testcases/test_logout.py
+++ testcases/test_logout.py
@@ -6,25 +6,14 @@ import time
 def test_10_verify_logout(driver):
 
     home_page = GuviHomePage(driver)
-
     logout_page = LogoutPage(driver)
-
     home_page.open_home_page()
-
     home_page.click_login_button()
-
     home_page.enter_email("vamsiviswanath60@gmail.com")
-
     home_page.enter_password("aspirineAS1@")
-
     home_page.click_login()
-
     time.sleep(2)
-
     logout_page.click_profile()
-
     time.sleep(2)
-
     logout_page.click_logout()
-
     assert "https://www.guvi.in/" == driver.current_url
